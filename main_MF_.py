8d29ecd1223c706469e7867668d9eeeff67cdc41
sereinexit <2174937273@qq.com>
2023-05-10 20:11:20 +0800

    MF-test

---
+++ main_MF_.py
@@ -0,0 +1,25 @@
+import sys
+sys.path.append("/home/wyf/MF-test")
+import os
+import yaml
+import time
+import numpy as np
+from models.mf import mf
+from pathlib import Path
+from scipy.sparse import load_npz
+path='/home/wyf/MF-test/'
+train_path = path+'data/train_user.npz'
+valid_path = path+'data/valid_user.npz'
+test_path = path+'data/test.npz'
+table_path = path+'tables/'
+opath = 'yahooR3/op_mf_tuning_u.csv'
+dataset = 'yahooR3/'
+
+train = load_npz(train_path).tocsr()
+validation = load_npz(valid_path).tocsr()
+test = load_npz(test_path).tocsr()
+
+RQ, X, xBias, Y, yBias = mf(train, validation, test, embeded_matrix=np.empty(0), iteration=500, seed=0, source=None,
+                            problem='yahooR3/', gpu_on=True, scene='u', metric='AUC', topK=50, is_topK=False,
+                            searcher='grid')
+
---
+++ utils/modelnames.py
@@ -0,0 +1,11 @@
+import sys
+sys.path.append("/home/wyf/MF-test")
+
+from models.dib_mf import dibmf
+
+
+models = {
+  
+    "DIB-MF": dibmf,
+
+}
